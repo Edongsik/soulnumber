a8596eebb201ae7400894eebce77b6ff4d473994
TaeHyun Kim <iusb0605@gmail.com>
2025-10-21 12:08:14 +0900

    Add GitHub Actions workflow for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/soulnumber/" // 👈 이 설정이 반드시 있어야 합니다!
 })
